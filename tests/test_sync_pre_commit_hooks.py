e98a9c7fcf08e990183bd23e98fdebfddfef985c
wpk-nist-gov <wpk@nist.gov>
2026-03-25 19:21:35 -0400

    chore: resolve conflicts

--- tests/test_sync_pre_commit_hooks.py
+++ tests/test_sync_pre_commit_hooks.py
@@ -7,22 +7,3 @@ def test_version() -> None:
     from sync_pre_commit_hooks import __version__
 
     assert __version__ != "999"
-<<<<<<< before updating
-=======
-
-
-@pytest.fixture
-def response() -> tuple[int, int]:
-    return 1, 2
-
-
-def test_example_function(response: tuple[int, int]) -> None:
-    expected = 3
-    assert example_function(*response) == expected
-
-
-def test_command_line_interface() -> None:
-    from sync_pre_commit_hooks import cli
-
-    assert not cli.main([])
->>>>>>> after updating
