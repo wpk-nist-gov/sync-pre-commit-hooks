d1a0d57d71af5e8f839170df9d53990841cc320a
wpk-nist-gov <5348008+wpk-nist-gov@users.noreply.github.com>
2026-07-24 14:00:26 +0000

    chore(template): accept new copier update [skip ci]

--- src/sync_pre_commit_hooks/__init__.py
+++ src/sync_pre_commit_hooks/__init__.py
@@ -6,7 +6,13 @@ Top level API (:mod:`sync_pre_commit_hooks`)
 from importlib.metadata import PackageNotFoundError
 from importlib.metadata import version as _version
 
+<<<<<<< before updating
 try:  # noqa: RUF067
+=======
+from .core import example_function
+
+try:  # ruff:ignore[non-empty-init-module]
+>>>>>>> after updating
     __version__ = _version("sync-pre-commit-hooks")
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "999"
